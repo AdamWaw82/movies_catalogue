6020dc58b7bb801572526c2f2f600bc46fba6e9a
POL0052246 <a.wawerski@auchan.pl>
2024-08-12 10:42:23 +0200

    Add lits type, navigation buttons, validation

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
 
 from movies_catalogue import tmdb_client
-from movies_catalogue.tmdb_client import get_movies
+from movies_catalogue.tmdb_client import get_movies, get_single_movie, get_single_movie_cast, get_random_image, get_list
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    movies = get_movies(how_many=8)
-    return render_template("homepage.html", movies=movies)
+    lists = get_list()
+    selected_list = request.args.get('list_type', 'popular')
+    movies = get_movies(how_many=8, list_name=selected_list)
+    return render_template("homepage.html", movie_lists=lists, movies=movies, current_list=selected_list)
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = get_single_movie(movie_id)
+    d_credits = get_single_movie_cast(movie_id)
+    image = get_random_image(movie_id)
+    print(image)
+    return render_template("movie_details.html", movie_image=image, movie=details, credits=d_credits["cast"])
 
 
 @app.context_processor
--- tmdb_client.py
+++ tmdb_client.py
@@ -2,16 +2,33 @@ import random
 
 import requests
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDVkNjUxMTZhMDFkMDNhZmRiNzMyNTA4ZmY4ZGY4MCIsIm5iZiI6MTcyMzQ0MzQ5Ny44MDk5NSwic3ViIjoiNjZiMzIwNDY3MTg1ODczN2YwNTk2ZjAzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.F3NJ1GmzLNKhoTGOS_XolCj1Dcpf2Fz7MoxI73hcib0"
+
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDVkNjUxMTZhMDFkMDNhZmRiNzMyNTA4ZmY4ZGY4MCIsIm5iZiI6MTcyMzQ0MzQ5Ny44MDk5NSwic3ViIjoiNjZiMzIwNDY3MTg1ODczN2YwNTk2ZjAzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.F3NJ1GmzLNKhoTGOS_XolCj1Dcpf2Fz7MoxI73hcib0"
     headers = {
-        "Authorization": f"Bearer {api_token}"
+        "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
 
+    return response.json()
+
+
+def get_list():
+    return [{"key": "now_playing", "value": "Now Playing"}
+        , {"key": "popular", "value": "Popular"}
+        , {"key": "top_rated", "value": "Top Rated"}
+        , {"key": "upcoming", "value": "Upcoming"}]
+
+
+def get_movies_list(list_name):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_name}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
     return response.json()
 
 
@@ -20,9 +37,47 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movies(how_many, list_name='popular'):
+    if list_name not in [item.get("key") for item in get_list()]:
+        list_name = 'popular'
+
+    data = get_movies_list(list_name)
     data_results = data["results"]
 
     shuffle_list = random.sample(data_results, how_many)
     return shuffle_list
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3//movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_image_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_random_image(movie_id):
+    data = get_image_movie(movie_id)
+    data_results = data["backdrops"]
+
+    random_image = random.sample(data_results, 1)[0]
+    return random_image
