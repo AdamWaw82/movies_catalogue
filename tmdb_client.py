5c5125aeb48abaeab490619b183035da2de8d049
Adam Wawerski <adamw467@gmail.com>
2024-09-09 08:03:50 +0200

    Add requirements.txt

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,8 +1,8 @@
 import random
-
+import os
 import requests
 
-API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDVkNjUxMTZhMDFkMDNhZmRiNzMyNTA4ZmY4ZGY4MCIsIm5iZiI6MTcyMzQ0MzQ5Ny44MDk5NSwic3ViIjoiNjZiMzIwNDY3MTg1ODczN2YwNTk2ZjAzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.F3NJ1GmzLNKhoTGOS_XolCj1Dcpf2Fz7MoxI73hcib0"
+API_TOKEN = os.environ.get("TMDB_API_TOKEN", "")
 
 
 def call_tmdb_api(endpoint):
